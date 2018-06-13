de9c4e465f419a3fbef0a226ba4b7d483a4e604e
Bart32 <bartosz.kosiec@gmail.com>
2018-06-13 19:35:09 +0200

    Added Thumbnails

--- js/script.js
+++ js/script.js
@@ -1,23 +1,52 @@
 window.onload = function()
 {
+
+
+
+
   var sliderDiv = document.getElementById("slider");
+  var thumbnailsDiv = document.getElementById("thumbnails");
   var images = ['css.png', 'html.png', 'javascript.png'];
 
 
   for(var i=0; i < images.length; i++){
     var slide = new Image();
+    var thumbnail = new Image();
+    thumbnail.src = "img/"+images[i];
     slide.src="img/"+images[i];
+    thumbnailsDiv.appendChild(thumbnail);
     sliderDiv.appendChild(slide);
   }
 
+
+
   var imagesElements = sliderDiv.getElementsByTagName('img');
+  var thumbnailsElements = thumbnailsDiv.getElementsByTagName('img');
+
+
 
   imagesElements[0].setAttribute("class", "active");
-  var i = 0;
-  
-  setInterval(function(){
-    imagesElements[i % 3].setAttribute("class", " ");
-    imagesElements[(i + 1) % 3].setAttribute("class", "active");
+  var k = 0;
+  var sliderInterval = setInterval(function(){
+    imagesElements[k % 3].setAttribute("class", " ");
+    imagesElements[(k + 1) % 3].setAttribute("class", "active");
     i++;
   },4500);
+
+  for (var j = 0; j <= thumbnailsElements.length-1; j++)
+ {
+ thumbnailsElements[j].onclick = function()
+  {
+          clearInterval(sliderInterval);
+          document.getElementsByClassName('active')[0].setAttribute("class", " ");
+          var index = Array.from(this.parentElement.children).indexOf(this) 
+          imagesElements[index].setAttribute("class", "active");
+          k=index;
+          sliderInterval = setInterval(function(){
+            imagesElements[k % 3].setAttribute("class", " ");
+            imagesElements[(k + 1) % 3].setAttribute("class", "active");
+            k++;
+          },4500);
+  }
+ };
 };
